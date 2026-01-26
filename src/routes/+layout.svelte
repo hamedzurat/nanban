29385fba66c1561f7acb4a8cf740fea4450c32bc
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2026-01-27 02:12:28 +0600

    :)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,11 @@
   import { setupConvex } from 'convex-svelte';
   import { ModeWatcher } from 'mode-watcher';
 
-  import { PUBLIC_CONVEX_URL } from '$env/static/public';
-
   import './layout.css';
 
   const { children } = $props();
 
-  const convexUrl = PUBLIC_CONVEX_URL || 'https://capable-pelican-238.convex.cloud';
-  $inspect(convexUrl);
-  setupConvex(convexUrl);
+  setupConvex('https://capable-pelican-238.convex.cloud');
 </script>
 
 <ModeWatcher />
