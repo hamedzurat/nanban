2a4ad41d4e0ea21aab3b3e7005a39debeabba9d8
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2026-01-26 17:13:57 +0600

    fix: favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
-  import './layout.css';
-
   import { setupConvex } from 'convex-svelte';
 
   import { PUBLIC_CONVEX_URL } from '$env/static/public';
 
-  import favicon from '$lib/assets/favicon.svg';
+  import './layout.css';
 
   const { children } = $props();
 
   setupConvex(PUBLIC_CONVEX_URL);
 </script>
-
-<svelte:head>
-  <link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
